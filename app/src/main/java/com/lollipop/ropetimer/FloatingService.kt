c4fac27b53053d085e1ff0c07678b22247b688c4
Lollipop <liujian594@hotmail.com>
2023-04-07 23:01:56 +0800

    开始写悬浮窗的服务需要做成前台服务

--- app/src/main/java/com/lollipop/ropetimer/FloatingService.kt
+++ app/src/main/java/com/lollipop/ropetimer/FloatingService.kt
@@ -1,16 +1,21 @@
 package com.lollipop.ropetimer
 
+import android.Manifest
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.IBinder
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 class FloatingService : Service() {
 
     companion object {
         private const val FLOATING_CHANNEL_ID = "Floating"
+        private const val FLOATING_NOTIFICATION_ID = 2333
     }
 
     override fun onBind(intent: Intent?): IBinder? {
@@ -50,7 +55,27 @@ class FloatingService : Service() {
     }
 
     private fun updateNotification() {
-        // TODO
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        NotificationManagerCompat.from(this).notify(
+            FLOATING_NOTIFICATION_ID,
+            NotificationCompat.Builder(this, FLOATING_CHANNEL_ID)
+                // TODO
+                .build()
+        )
+
     }
 
 }
