c2b385babdda42b16550ad632313e6c3f730579e
Lollipop <liujian594@hotmail.com>
2023-04-14 22:45:14 +0800

    协议的管理页面和协议的编辑页面写了个框，但是没想好怎么写

--- app/src/main/java/com/lollipop/ropetimer/ProtocolEditActivity.kt
+++ app/src/main/java/com/lollipop/ropetimer/ProtocolEditActivity.kt
@@ -3,6 +3,9 @@ package com.lollipop.ropetimer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.lollipop.ropetimer.databinding.ActivityProtocolEditBinding
+import com.lollipop.ropetimer.utils.insets.WindowInsetsEdge
+import com.lollipop.ropetimer.utils.insets.WindowInsetsHelper
+import com.lollipop.ropetimer.utils.insets.fixInsetsByPadding
 
 class ProtocolEditActivity : AppCompatActivity() {
 
@@ -11,7 +14,11 @@ class ProtocolEditActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowInsetsHelper.initWindowFlag(this)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.actionBar.root.fixInsetsByPadding(WindowInsetsEdge.HEADER)
+        binding.actionBar.backButton.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        binding.actionBar.titleView.setText(R.string.plan_manager)
     }
 }
--- app/src/main/java/com/lollipop/ropetimer/ProtocolManagerActivity.kt
+++ app/src/main/java/com/lollipop/ropetimer/ProtocolManagerActivity.kt
@@ -2,7 +2,12 @@ package com.lollipop.ropetimer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.lollipop.ropetimer.databinding.ActivityProtocolManagerBinding
+import com.lollipop.ropetimer.utils.insets.WindowInsetsEdge
+import com.lollipop.ropetimer.utils.insets.WindowInsetsHelper
+import com.lollipop.ropetimer.utils.insets.fixInsetsByPadding
 
 class ProtocolManagerActivity : AppCompatActivity() {
 
@@ -11,7 +16,22 @@ class ProtocolManagerActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowInsetsHelper.initWindowFlag(this)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.actionBar.root.fixInsetsByPadding(WindowInsetsEdge.HEADER)
+        binding.actionBar.backButton.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        binding.actionBar.titleView.setText(R.string.plan_manager)
+        binding.refreshLayout.setOnRefreshListener {
+            loadData()
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        // TODO Adapter
+        loadData()
     }
+
+    private fun loadData() {
+        // TODO
+    }
+
 }
